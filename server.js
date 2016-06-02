4e5d5ce0c0171f6989cdcea2fc5020979419820f
nampei kamata <k0408n@yahoo.co.jp>
2016-06-02 16:50:50 +0900

    web pack

--- server.js
+++ server.js
@@ -1,88 +1,23 @@
-'use strict';
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-var _express = require('express');
+var app = new (require('express'))()
+var port = 3000
 
-var _express2 = _interopRequireDefault(_express);
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-var _path = require('path');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-var _path2 = _interopRequireDefault(_path);
-
-var _morgan = require('morgan');
-
-var _morgan2 = _interopRequireDefault(_morgan);
-
-var _cookieParser = require('cookie-parser');
-
-var _cookieParser2 = _interopRequireDefault(_cookieParser);
-
-var _bodyParser = require('body-parser');
-
-var _bodyParser2 = _interopRequireDefault(_bodyParser);
-
-var _ejs = require('ejs');
-
-var _ejs2 = _interopRequireDefault(_ejs);
-
-var _index = require('./routes/index');
-
-var _index2 = _interopRequireDefault(_index);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-// import users from './routes/users'
-// import create from './routes/create'
-// import linebot from './routes/linebot'
-
-var app = (0, _express2.default)();
-
-app.engine('ejs', _ejs2.default.renderFile);
-app.set('view engine', 'ejs');
-
-// view engine setup
-app.set('views', _path2.default.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-app.use((0, _morgan2.default)('dev'));
-app.use(_bodyParser2.default.json());
-app.use(_bodyParser2.default.urlencoded({ extended: false }));
-app.use((0, _cookieParser2.default)());
-app.use(_express2.default.static(_path2.default.join(__dirname, 'dist/public')));
-
-app.use('/', _index2.default);
-// app.use('/users', users)
-// app.use('/create', create)
-// app.use('/linebot/callback', linebot)
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
-module.exports = app;
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
