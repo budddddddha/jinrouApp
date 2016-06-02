24549ddfeda126a0723fce1095680b6a6fdebc07
nampei kamata <k0408n@yahoo.co.jp>
2016-06-03 00:33:04 +0900

    routing first cons

--- reducers/index.js
+++ reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import todos from './todos'
 import login from './login'
+import { routerReducer } from 'react-router-redux'
 
 const rootReducer = combineReducers({
   todos,
-  login
+  login,
+  routing: routerReducer
 })
 
 export default rootReducer
